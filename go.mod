649878056c38b7569367418a17cb4413f7a63c6b
kulshreshth khare <sunnykhareu@gmail.com>
2022-09-14 02:43:22 +0530

    added graceful shutdown

--- go.mod
+++ go.mod
@@ -28,9 +28,9 @@ require (
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220722155302-e5dcc9cfc0b9 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+	"fmt"
+
 	"example.com/dockermanager/configs"
 	"example.com/dockermanager/routes"
 	"github.com/docker/docker/client"
@@ -15,6 +22,22 @@ func main() {
 	//setup routes
 	routes.SetupRoutes(e)
 
-	//start server
-	e.Logger.Fatal(e.Start(":" + configs.GetEnvVar("APP_PORT")))
+	// Start server
+	go func() {
+		if err := e.Start(":" + configs.GetEnvVar("APP_PORT")); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 20 seconds. 
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	sig := <-quit
+	fmt.Println("Received terminate, graceful shutdown", sig)
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
