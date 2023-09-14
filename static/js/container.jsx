549a8cbd54d1846d3c4b0641930bb0391f4e3944
kulshreshth khare <sunnykhareu@gmail.com>
2023-09-15 00:53:21 +0530

    Updated react component and taken out menu in common jsx

--- static/js/container.jsx
+++ static/js/container.jsx
@@ -23,37 +23,6 @@ class App extends React.Component {
     }
 }
 
-class Menu extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {routeState: '/containers'};
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(menuURL) {
-        this.setState({routeState: menuURL});
-    }
-
-    render() {
-        const menus = [
-            {"menuName": "Containers", "menuURL": "/containers"},
-            {"menuName": "Volumes", "menuURL": "/volumes"},
-            {"menuName": "Images", "menuURL": "/images"},
-            {"menuName": "Help", "menuURL": "/help"}
-        ]
-    
-        return <div className="col-md-3">
-            <div className="list-group">
-                {menus.map((menu) =>
-                    <a href={menu.menuURL} onClick={this.handleClick.bind(this, menu.menuURL)} className="list-group-item" key={menu.menuName}>{menu.menuName}</a>
-                )}
-            </div>
-        </div>;
-    }
-}
-
 class ContentWindow extends React.Component {
 
     constructor(props) {
